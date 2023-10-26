289f42804d1d4348e8cd7b52c638a3958c9148d9
clock zhong <zhong.clock@gmail.com>
2023-10-26 15:04:32 +0800

    added cztest/test_sb3.py into project:stable-baselines3

---
+++ cztest/test_sb3.py
@@ -0,0 +1,37 @@
+import importlib.util
+import os
+import sys
+import time
+import pytest
+
+
+class MimicClass():
+    test_int: int
+    test_float: float
+    test_string: str = "test_string............"
+    def __init__(self):
+        self.test_int = 323234
+        self.test_float = 2.335235
+
+def test_TestClass():
+    test_obj1 = MimicClass()
+    test_obj2 = MimicClass()
+    print(test_obj1.test_float, test_obj2.test_float)
+    print(test_obj1.test_int, test_obj2.test_int)
+    print(test_obj1.test_string, test_obj2.test_string)
+    test_obj1.test_int = 1
+    assert test_obj1.test_int != test_obj2.test_int
+    test_obj2.test_float = 0.332
+    assert test_obj1.test_float != test_obj2.test_float
+    test_obj2.test_string = "234asfasdfasfas"
+
+    #test_string is not shared for all instances from MimicClass, it's also instance variables
+    #assert test_obj1.test_string == test_obj2.test_string
+
+    assert test_obj1.test_string != test_obj2.test_string
+    
+    print(test_obj1.test_float, test_obj2.test_float)
+    print(test_obj1.test_int, test_obj2.test_int)
+    print(test_obj1.test_string, test_obj2.test_string)
+
+    assert MimicClass.test_string == test_obj1.test_string
